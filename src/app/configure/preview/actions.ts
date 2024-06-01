2ef4de3247268132ba4f40cba725d7f84bb21149
Ryan <tangguangran@gmail.com>
2024-05-31 20:23:17 -0500

    Added relevant data to be added to prisma, created actions up till implementing Stripe in actions.ts for preview, and need to work on useMutation in DesignPreview.tsx after finishing actions.ts.

---
+++ src/app/configure/preview/actions.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Order } from "@prisma/client";
+
+export const createCheckoutSession = async ({
+  configId,
+}: {
+  configId: string;
+}) => {
+  const configuration = await db.configuration.findUnique({
+    where: { id: configId },
+  });
+
+  if (!configuration) {
+    throw new Error("No such configuration found");
+  }
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    throw new Error("You need to be logged in");
+  }
+
+  //   Here we are calculating the total price server side to prevent data manipulation on the client side.
+  const { finish, material } = configuration;
+
+  let price = BASE_PRICE;
+  if (finish === "textured") price += PRODUCT_PRICES.finish.textured;
+  if (material === "polycarbonate")
+    price += PRODUCT_PRICES.material.polycarbonate;
+
+  let order: Order | undefined = undefined;
+
+  const existingOrder = await db.order.findFirst({
+    where: {
+      userId: user.id,
+      configurationId: configuration.id,
+    },
+  });
+};
